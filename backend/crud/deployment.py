e8ab48658996ff06dd32e91299c1802fdccd413a
maciekrepository <macieklazarz@gmail.com>
2025-09-23 13:11:28 +0200

    sqlitedb

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -34,6 +34,8 @@ STORAGES = {
 CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
 
+
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -43,6 +45,7 @@ DATABASES = {
         "PASSWORD": CONNECTION_STR['password'],
     }
 }
+'''
 
 LOGGING = {
     'version': 1,
